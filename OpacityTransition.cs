00a16b92c9563a8557d15b12bd72e18ecdc62f96
Alex <48759429+Aurumaker72@users.noreply.github.com>
2022-04-06 17:45:42 +0200

    Add files

---
+++ OpacityTransition.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace WPFBareAnimations
+{
+    public static class OpacityTransition
+    {
+
+        public static readonly DependencyProperty ShouldBeShownProperty = DependencyProperty.RegisterAttached(
+            "ShouldBeShown",
+            typeof(bool),
+            typeof(OpacityTransition),
+            new PropertyMetadata(default(bool), OnShouldBeShownChanged));
+
+        public static readonly DependencyProperty AnimationDurationProperty = DependencyProperty.Register("AnimationDuration", typeof(TimeSpan), typeof(OpacityTransition));
+
+        public static void SetAnimationDuration(DependencyObject element, TimeSpan value) => element.SetValue(AnimationDurationProperty, value);
+        [AttachedPropertyBrowsableForType(typeof(DependencyObject))]
+        public static TimeSpan GetAnimationDuration(DependencyObject element) => (TimeSpan)element.GetValue(AnimationDurationProperty);
+
+        public static void SetShouldBeShown(DependencyObject element, bool value) => element.SetValue(ShouldBeShownProperty, value);
+        [AttachedPropertyBrowsableForType(typeof(DependencyObject))]
+        public static bool GetShouldBeShown(DependencyObject element) => (bool)element.GetValue(ShouldBeShownProperty);
+
+        private static void OnShouldBeShownChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue as bool? == null) throw new ArgumentNullException("Can\'t be null");
+            var target = d as FrameworkElement;
+            var duration = GetAnimationDuration(d) == null ? TimeSpan.FromMilliseconds(500) : GetAnimationDuration(d);
+            var opacityAnimation = new DoubleAnimation
+            {
+                To = (bool)e.NewValue ? 1 : 0,
+                Duration = duration,
+            };
+            var visibilityAnimation = new ObjectAnimationUsingKeyFrames();
+            visibilityAnimation.KeyFrames.Add(new DiscreteObjectKeyFrame()
+            {
+                KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0)),
+                Value = Visibility.Visible
+            });
+            visibilityAnimation.KeyFrames.Add(new DiscreteObjectKeyFrame()
+            {
+                KeyTime = KeyTime.FromTimeSpan(duration),
+                Value = (bool)(e.NewValue) ? Visibility.Visible : Visibility.Collapsed
+            });
+
+            var hostStoryboard = new Storyboard()
+            {
+                Children = new TimelineCollection(new Timeline[] { opacityAnimation, visibilityAnimation })
+            };
+            Storyboard.SetTarget(opacityAnimation, target);
+            Storyboard.SetTarget(visibilityAnimation, target);
+            Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath(UIElement.OpacityProperty));
+            Storyboard.SetTargetProperty(visibilityAnimation, new PropertyPath(UIElement.VisibilityProperty));
+            hostStoryboard.Begin();
+        }
+
+    }
+}
